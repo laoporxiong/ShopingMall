9a256001345620bdbe72230713f2b4433144568f
laoporxiong <lalpxxiong62@gmail.com>
2021-10-21 15:10:48 +0700

    Update today

---
+++ lib/state/authen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:shopingmall/utility/my_constant.dart';
+import 'package:shopingmall/widget/show_image.dart';
+import 'package:shopingmall/widget/show_title.dart';
+
+class Authen extends StatefulWidget {
+  const Authen({Key? key}) : super(key: key);
+
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+
+bool statusRedEye = true;
+
+  @override
+  Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            buidImage(size),
+            buildAppName(),
+            buildUser(size),
+            buildPassword(size)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h2Style(),
+              labelText: 'User :',
+              prefixIcon: Icon(
+                Icons.account_circle,
+                color: MyConstant.ligth,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dart),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.ligth),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField( obscureText: statusRedEye,
+            decoration: InputDecoration( 
+              labelStyle: MyConstant().h2Style(),
+              labelText: 'Password :',
+              prefixIcon: Icon(
+                Icons.lock_clock_outlined,
+                color: MyConstant.ligth,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dart),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.ligth),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(title: MyConstant.appName, textStyle: MyConstant().h1Style()),
+      ],
+    );
+  }
+
+  Row buidImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: ShowImage(
+            pathImage: MyConstant.image4,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'Shopping Mall';
+
+  //Route
+  static String rounteAuthen = '/authen';
+  static String rounteCreateAccount = '/createAccount';
+  static String rounteBuyerService = '/buyerService';
+  static String rounteSellerService = '/sellerService';
+  static String rounteRiderService = '/riderService';
+
+  //Image
+
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
+
+  //Style
+  TextStyle h1Style() =>
+      TextStyle(fontSize: 25, color: dart, fontWeight: FontWeight.bold);
+      TextStyle h2Style() =>
+      TextStyle(fontSize: 18, color: dart, fontWeight: FontWeight.w700);
+      TextStyle h3Style() =>
+      TextStyle(fontSize: 12, color: dart, fontWeight: FontWeight.normal);
+
+  //Color
+  static Color primary = Color(0xffab47bc);
+  static Color dart = Color(0xff790e8b);
+  static Color ligth = Color(0xffdf78ef);
+}
